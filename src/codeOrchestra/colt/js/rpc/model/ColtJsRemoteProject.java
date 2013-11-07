e0eb58f1a9a4273e34b024d9cac0693bdb5aed89
Alexander Eliseyev <a.a.eliseyev@gmail.com>
2013-11-07 17:26:39 +0400

    [CJ-825]

--- src/codeOrchestra/colt/js/rpc/model/ColtJsRemoteProject.java
+++ src/codeOrchestra/colt/js/rpc/model/ColtJsRemoteProject.java
@@ -7,26 +7,7 @@ import codeOrchestra.colt.core.rpc.model.ColtRemoteProject;
  */
 public class ColtJsRemoteProject extends ColtRemoteProject {
 
-    private String path;
-    private String name;
-
     private String mainDocument;
-
-    public String getPath() {
-        return path;
-    }
-
-    public void setPath(String path) {
-        this.path = path;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public String getMainDocument() {
         return mainDocument;
