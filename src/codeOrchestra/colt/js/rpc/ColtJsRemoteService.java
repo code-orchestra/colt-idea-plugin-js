1790e3399100f026ddc45bf28d4ed44b4d4d2ecc
Alexander Eliseyev <a.a.eliseyev@gmail.com>
2013-11-11 14:55:14 +0400

    [CJ-825]

---
+++ src/codeOrchestra/colt/js/rpc/ColtJsRemoteService.java
@@ -0,0 +1,19 @@
+package codeOrchestra.colt.js.rpc;
+
+import codeOrchestra.colt.core.rpc.ColtRemoteService;
+import codeOrchestra.colt.core.rpc.ColtRemoteTransferableException;
+
+/**
+ * @author Alexander Eliseyev
+ */
+public interface ColtJsRemoteService extends ColtRemoteService {
+
+    // Secured methods
+
+    void startLive(String securityToken) throws ColtRemoteTransferableException;
+
+    void startProduction(String securityToken) throws ColtRemoteTransferableException;
+
+    String getContextForPosition(String securityToken, String file, int position);
+
+}
